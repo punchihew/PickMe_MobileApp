7b465e608a48acbabb8290e4941a8dc861af3c5f
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-09 23:02:48 +0530

    updeta

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text ,StyleSheet} from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View style={styles.container}> 
+      <Text   >Hi</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container:{
+    
+  }
+
+});
