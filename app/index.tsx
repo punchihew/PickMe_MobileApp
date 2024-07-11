7a7eef5fbc425db7f550cb7006c75659dc0524df
punchihewa <navishkapunchihewa66@gmail.com>
2024-07-11 15:11:41 +0530

    updeta

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,36 @@
-import { View, Text,SafeAreaView,StyleSheet } from 'react-native'
+import { View, Text,SafeAreaView,StyleSheet,TouchableOpacity,ImageBackground } from 'react-native'
 import React from 'react'
 
 export default function index() {
   return (
-    <SafeAreaView style = {styles.container}>
-       <View>
+    <SafeAreaView>
+       <View style = {styles.container}>
+       <TouchableOpacity style={styles.button1} onPress={() => alert('Button pressed!')} >
+            <ImageBackground
+            source={{
+                uri: 'https://img.icons8.com/?size=100&id=19175&format=png&color=000000',
+            }}
+            style={styles.imageBackground}
+            imageStyle={styles.imageStyle}
+            >
+            </ImageBackground>
+            
+        </TouchableOpacity>
+        <Text style ={styles.Text}>Clear Card</Text>
+       </View>
 
+       <View style={styles.container1}>
+       <TouchableOpacity style={styles.button2} onPress={() => alert('Button pressed!')} >
+            <ImageBackground
+            source={{
+                uri: 'https://img.icons8.com/?size=100&id=19175&format=png&color=000000',
+            }}
+            style={styles.imageBackground1}
+            imageStyle={styles.imageStyle1}
+            >
+            </ImageBackground>
+            
+        </TouchableOpacity>
        </View>
     </SafeAreaView>
   )
@@ -14,10 +39,54 @@ export default function index() {
 const styles = StyleSheet.create({
 
     container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#FEF5E6', 
-        position: 'relative'
+        width: 390,
+        height: 50,
+        backgroundColor: 'white', 
+        position: 'relative',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5, // for Android
+        marginTop:10,
+      },
+      button1: {
+        width: 32,
+        height: 36,
+        borderRadius: 10,
+        overflow: 'hidden',
+        marginTop:8,
+        marginLeft:10,
+        display:"flex"
       },
 
+      imageBackground: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      imageStyle: {
+        borderRadius: 10,
+      },    
+      Text:{
+            color: 'red',
+            fontWeight: 'bold',
+            marginLeft:310,
+            marginTop:-28,
+      },
+      container1: {
+        width: 390,
+        height: 640,
+        backgroundColor: 'white', 
+        position: 'relative',
+        shadowColor: 'orange',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 5, // for Android
+        marginTop:10,
+      },
+      
+
     })
+       
